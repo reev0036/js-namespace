cc396d0a426719dee19adeb48e202979d3aa015f
reev0036 <32390685+reev0036@users.noreply.github.com>
2017-10-16 11:58:22 -0400

    Color changes

--- namespace.js
+++ namespace.js
@@ -1,5 +1,5 @@
 var REEV0036 = {
-    init:function() {
+    init: function () {
         var div = document.createElement("div");
         div.className = "box";
         div.textContent = "reev0036";
@@ -10,16 +10,18 @@ var REEV0036 = {
         div.addEventListener("mouseout", div_mouseOut);
 
         function div_click(event) {
-          event.currentTarget.style.backgroundColor = "#5691bb";
-          event.currentTarget.style.borderColor = "#28547f";
+            event.currentTarget.style.backgroundColor = "#5691bb";
+            event.currentTarget.style.borderColor = "#28547f";
+            event.currentTarget.style.color = "white";
+            event.currentTargen.style.textShadow = "1px 1px rbga(0,0,0,0.7)"
         }
 
         function div_mouseOver(event) {
-          event.currentTarget.classList.toggle("highlight");
+            event.currentTarget.classList.toggle("highlight");
         }
 
         function div_mouseOut(event) {
-          event.currentTarget.classList.toggle("highlight");
+            event.currentTarget.classList.toggle("highlight");
         }
     }
 };
