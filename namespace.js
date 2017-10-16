fe4fbb3b1c067e9da02816b04842f7a21e356d40
reev0036 <32390685+reev0036@users.noreply.github.com>
2017-10-16 11:51:48 -0400

    code changes and testing

--- namespace.js
+++ namespace.js
@@ -1,29 +1,27 @@
 var REEV0036 = {
-    init: function () {
-        
+    init:function() {
         var div = document.createElement("div");
         div.className = "box";
-        div.textContent = "REEV0036";
-        
-        var div_boxes = document.querySelector("#boxes");
-        div_boxes.appendChild("div");
-        
+        div.textContent = "reev0036";
+        document.getElementById("boxes").appendChild(div);
+
         div.addEventListener("click", div_click);
         div.addEventListener("mouseover", div_mouseOver);
         div.addEventListener("mouseout", div_mouseOut);
 
         function div_click(event) {
-            event.currentTarget.style.backgroundColor = "blue";
-            event.currentTarget.style.borderColor = "lightgreen";
+          event.currentTarget.style.backgroundColor = "blue";
+          event.currentTarget.style.borderColor = "grey";
         }
-        
+
         function div_mouseOver(event) {
-            event.currentTarget.classList.toggle("highlight");
+          event.currentTarget.classList.toggle("highlight");
+
         }
-        
+
         function div_mouseOut(event) {
-            event.currentTarget.classList.toggle("highlight");
+          event.currentTarget.classList.toggle("highlight");
+
         }
-        
     }
 };
